3c67982b17307378ff094612ce63959246fde30f
Kaburi <specialemail90@gmail.com>
2025-12-07 11:12:39 +0900

    .

--- src/types/api/user-setting.ts
+++ src/types/api/user-setting.ts
@@ -13,6 +13,8 @@ interface BaseUpsertUserSettingRequest {
   aCustomLevelPrompt?: string;
   pCustomLevelPrompt?: string;
   useAutoRecord?: boolean;
+  // 실험적 기능
+  ex_useProofreading?: boolean; // 문장 교정 기능 사용 여부
 }
 
 export type UpsertUserSettingRequest = Partial<BaseUpsertUserSettingRequest>;
